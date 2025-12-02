1bb867556cb2fc605ebb84d40eea23bff8f57b0a
oliveira1360 <98216329+oliveira1360@users.noreply.github.com>
2025-12-02 11:28:21 +0000

    make so invite have time expiration

--- service/src/main/kotlin/InviteService.kt
+++ service/src/main/kotlin/InviteService.kt
@@ -1,19 +1,25 @@
 package org.example
 
+import config.InviteDomainConfig
 import jakarta.inject.Named
 import org.example.entity.core.Invite
 import java.time.Clock
+import java.time.Duration
 import java.util.UUID
 
 @Named
 class InviteService(
     private val trxManager: TransactionManager,
     private val clock: Clock,
+    private val config: InviteDomainConfig,
 ) {
     private val expirationTimeInSeconds = 10L
     fun getInvite(invite: String): Invite? =
         trxManager.run {
             val result = repositoryGeneral.getInvite(invite) ?: return@run null
+            if (isInviteTimeValild(clock, result)){
+                return@run null
+            }
             result
         }
 
@@ -32,4 +38,14 @@ class InviteService(
             Success(result)
 
         }
+
+
+    private fun isInviteTimeValild(
+        clock: Clock,
+        invite: Invite,
+    ): Boolean {
+        val now = clock.instant()
+        return invite.createdAt <= now &&
+                Duration.between(now, invite.createdAt) <= config.tokenTtl
+    }
 }
