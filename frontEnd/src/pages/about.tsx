8fa9700b2fe707d35e9547300f3964dfbe4f0a5a
Jessé Alencar <161583712+jfmalencar@users.noreply.github.com>
2025-12-02 12:17:53 +0000

    Removed unused NavBar. Finished About Page. Updated Player Profile Page. Started development of update user operation.

--- frontEnd/src/pages/about.tsx
+++ frontEnd/src/pages/about.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
-import {NavBar} from '../components/navBar';
+import { playerService } from "../services/player/playerService";
+import { PlayerInfoResponse } from "../services/player/playerResponseTypes";
+import { userInfo } from "os";
 
 
 function AboutPage() {
     return (
-        <div>
-            <NavBar/>
+        <div className="home-container lobby-page">
             <h1>About Page</h1>
+            <div>
+                <p>
+                    This game was developed during the
+                    DAW (Desenvolvimento de Aplicações Web) course at ISEL (Instituto Superior de Engenharia de Lisboa).
+                </p>
+                
+                <p>
+                    The project was created by a team of dedicated students aiming to apply their knowledge in web development.
+                </p>
+                <strong>Team Members:</strong>
+                <ul>     
+                    <li>Diogo Oliveira (A51662)</li>
+                    <li>Paulo Nascimento (A51665)</li>
+                    <li>Jessé Alencar (A51745)</li>
+                </ul>
+            </div>
+            <div>
+                <p>&copy; 2025 DAW Poker Dice Project - ISEL</p>
+            </div>
         </div>
     );
 }
