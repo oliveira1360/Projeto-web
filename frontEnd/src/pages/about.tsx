5ade33ba917eabac7a38c7b3ca84e6e0429bab01
Jessé Alencar <161583712+jfmalencar@users.noreply.github.com>
2025-12-07 00:19:32 +0000

    Added Home button to the About Page. Refactored CSS for the About Page.

--- frontEnd/src/pages/about.tsx
+++ frontEnd/src/pages/about.tsx
@@ -1,31 +1,43 @@
 import * as React from "react";
-import { playerService } from "../services/player/playerService";
-import { PlayerInfoResponse } from "../services/player/playerResponseTypes";
-import { userInfo } from "os";
+import { Link } from "react-router";
+import "./about.css";
 
 
 function AboutPage() {
     return (
-        <div className="home-container lobby-page">
-            <h1>About Page</h1>
-            <div>
-                <p>
-                    This game was developed during the
-                    DAW (Desenvolvimento de Aplicações Web) course at ISEL (Instituto Superior de Engenharia de Lisboa).
-                </p>
-                
-                <p>
-                    The project was created by a team of dedicated students aiming to apply their knowledge in web development.
-                </p>
-                <strong>Team Members:</strong>
-                <ul>     
-                    <li>Diogo Oliveira (A51662)</li>
-                    <li>Paulo Nascimento (A51665)</li>
-                    <li>Jessé Alencar (A51745)</li>
-                </ul>
+        <div className="about-container">
+            <div className="about-header">
+                <h1>About</h1>
             </div>
-            <div>
-                <p>&copy; 2025 DAW Poker Dice Project - ISEL</p>
+
+            <nav className="about-nav">
+                <Link to="/home">Home</Link>
+            </nav>
+
+            <div className="about-card">
+                <div className="about-content">
+                    <p>
+                        This game was developed during the
+                        DAW (Desenvolvimento de Aplicações Web) course at ISEL (Instituto Superior de Engenharia de Lisboa).
+                    </p>
+                    
+                    <p>
+                        The project was created by a team of dedicated students aiming to apply their knowledge in web development.
+                    </p>
+                    
+                    <div className="about-team">
+                        <strong>Team Members:</strong>
+                        <ul>     
+                            <li>Diogo Oliveira (A51662)</li>
+                            <li>Paulo Nascimento (A51665)</li>
+                            <li>Jessé Alencar (A51745)</li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div className="about-footer">
+                    <p>&copy; 2025 DAW Poker Dice Project - ISEL</p>
+                </div>
             </div>
         </div>
     );
